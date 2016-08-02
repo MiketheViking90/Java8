593922dfda1aabc623048d5e29f6c2a00afad484
Sili Wen <siliwen@gmail.com>
2016-08-01 21:44:16 -0700

    Combine ZoneId with Temporal classes

--- src/chap12/timezone/Zones.java
+++ src/chap12/timezone/Zones.java
@@ -1,6 +1,10 @@
 package chap12.timezone;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.zone.ZoneRules;
 import java.util.TimeZone;
 
@@ -18,9 +22,22 @@ public class Zones {
         System.out.println(defaultTzToZone);
     }
 
+    void applyZoneId() {
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDate dt = LocalDate.of(1990, Month.NOVEMBER, 11);
+        ZonedDateTime zdt1 = dt.atStartOfDay(zone);
+        System.out.println(zdt1);
+
+        Instant now = Instant.now();
+        ZonedDateTime zdt2 = now.atZone(zone);
+        System.out.println(zdt2);
+    }
     public static void main(String[] args) {
         Zones zones = new Zones();
         zones.createTimeZone();
+        System.out.println();
+
+        zones.applyZoneId();
     }
 
 }
